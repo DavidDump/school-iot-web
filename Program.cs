5d173f40151d467e3197b71fe9bef7d3dbb542b7
David Benes <dbenes10@gmail.com>
2024-11-15 23:19:15 +0100

    added local ip binding

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ builder.Services.AddSingleton<LedController>();
 
 var app = builder.Build();
 
+app.Urls.Add("http://localhost:5000");
+app.Urls.Add("http://192.168.0.151:5000");
 app.UseStaticFiles();
 app.UseRouting();
 app.MapControllers();
